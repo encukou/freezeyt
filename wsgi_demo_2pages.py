108619c17d2fdc00d74a74b70b4f7961a24e5690
Petr Viktorin <encukou@gmail.com>
2020-07-13 18:39:46 +0200

    Delete wsgi_demo_2pages.py

--- wsgi_demo_2pages.py
+++
@@ -1,9 +0,0 @@
-from wsgiref.simple_server import make_server
-
-from demo_app_2pages import app
-
-with make_server('', 8000, app) as server:
-    print("Serving HTTP on port 8000...")
-
-    # Respond to requests until process is killed
-    server.serve_forever()
