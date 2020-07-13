022aee0c78432e8b622ae182dee49eb2df980127
Petra Cihalova <disi77@gmail.com>
2020-07-13 16:43:13 +0200

    Added new file with app and wsgi for testing 2 pages app

---
+++ demo_app_2pages.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return """
+    <html>
+        <head>
+            <title>Hello world</title>
+        </head>
+        <body>
+            Hello world!
+            <br>
+            <a href='/second_page'>LINK</a> to second page.
+        </body>
+    </html>
+    """
+
+
+@app.route('/second_page')
+def second_page():
+    return """
+    <html>
+        <head>
+            <title>Hello world second page</title>
+        </head>
+        <body>
+            Second page !!!
+        </body>
+    </html>
+    """
