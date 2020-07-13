22601b42d2a2465541537a45625689d9d6587996
Petra Cihalova <disi77@gmail.com>
2020-07-13 16:43:35 +0200

    Updated test for 2 pages app

--- test_demo.py
+++ test_demo.py
@@ -1,7 +1,10 @@
 import pytest
+
 from demo_app import app
+from demo_app_2pages import app as app_2pages
 
 from freezeyt import freeze
+
 
 def test_one_page(tmp_path):
     freeze(app, tmp_path)
@@ -16,6 +19,40 @@ def test_one_page(tmp_path):
         </head>
         <body>
             Hello world!
+        </body>
+    </html>
+    """
+
+
+def test_two_pages(tmp_path):
+    freeze(app_2pages, tmp_path)
+
+    with open(tmp_path / "index.html", encoding='utf-8') as f:
+        read_text = f.read()
+
+    assert read_text == """
+    <html>
+        <head>
+            <title>Hello world</title>
+        </head>
+        <body>
+            Hello world!
+            <br>
+            <a href='/second_page'>LINK</a> to second page.
+        </body>
+    </html>
+    """
+
+    with open(tmp_path / "second_page.html", encoding='utf-8') as f2:
+        read_text2 = f2.read()
+
+    assert read_text2 == """
+    <html>
+        <head>
+            <title>Hello world second page</title>
+        </head>
+        <body>
+            Second page !!!
         </body>
     </html>
     """
