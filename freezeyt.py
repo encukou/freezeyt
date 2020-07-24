5eda7c2b4903f443fe4a14e26334401d0b37d0c4
David Stroch <d.stroch@gmail.com>
2020-07-24 09:49:40 +0200

    Update module freezeyt for better efficient via set type
    - rewrite all lists for links from type list to type set
    Set type is more suitable when we doesnt want list with duplicates.
    So WHILE cycle doesn't have to run many times.

--- freezeyt.py
+++ freezeyt.py
@@ -12,9 +12,9 @@ def freeze(app, path):
         print('status', status)
         print('headers', headers)
 
-    links = ['/']
+    links = set('/')
 
-    visited_links = []
+    visited_links = set()
 
     while links:
         link = links.pop()
@@ -22,7 +22,7 @@ def freeze(app, path):
         if link in visited_links:
             continue
 
-        visited_links.append(link)
+        visited_links.add(link)
 
         print(link)
 
@@ -42,7 +42,7 @@ def freeze(app, path):
                 f.write(item)
 
         with open(url_to_filename(path, link), "rb") as f:
-            links.extend(get_all_links(f))
+            links = links.union(set(get_all_links(f)))
 
 
 def get_all_links(page_content):
@@ -57,10 +57,10 @@ def get_all_links(page_content):
 def get_links_from_node(node):
     """Get all links from xml.dom.minidom Node
     """
-    result = []
+    result = set()
     if 'href' in node.attrib:
-        result.append(node.attrib['href'])
+        result.add(node.attrib['href'])
     for child in node:
-        result.extend(get_links_from_node(child))
+        result = result.union(get_links_from_node(child))
     return result
 
