ea1ba84c7e9f30f551488a121fc81183c9ccf3a1
Petr Viktorin <encukou@gmail.com>
2020-07-27 19:57:49 +0200

    Add tests for url_to_filename

---
+++ test_url_to_filename.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import pytest
+
+from freezeyt import url_to_filename
+
+base = Path('/test/')
+
+
+def test_index():
+    assert url_to_filename(base, "/") == base / "index.html"
+
+
+def test_second_page_html():
+    result = url_to_filename(base, "/second_page.html")
+    assert result == base / "second_page.html"
+
+
+def test_second_page():
+    result = url_to_filename(base, "/second/")
+    assert result == base / "second/index.html"
+
+
+def test_absolute():
+    result = url_to_filename(base, "http://localhost:8000/")
+    assert result == base / "index.html"
+
+
+def test_absolute_html():
+    result = url_to_filename(base, "http://localhost:8000/second.html")
+    assert result == base / "second.html"
+
+
+def test_external_page():
+    with pytest.raises(ValueError):
+        url_to_filename(base, "http://python.cz")
