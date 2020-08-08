9c088139f2500fb94bf4b67a162ba7995eb220b6
Adel Popelkova <adel.popelek@gmail.com>
2020-08-08 13:50:01 +0200

    Add first version of CLI help

--- freezeyt/cli.py
+++ freezeyt/cli.py
@@ -9,6 +9,17 @@ from freezeyt.freezing import freeze
 @click.argument('module_name')
 @click.argument('dest_path')
 def main(module_name, dest_path):
+    """
+    MODULE_NAME
+        Name of the Python web app module which will be frozen.
+
+    DEST_PATH
+        Absolute or relative path to the directory to which the files
+    will be frozen.
+
+    Example use:
+        python -m freezeyt demo_app build
+    """
     module = importlib.import_module(module_name)
     app = module.app
 
