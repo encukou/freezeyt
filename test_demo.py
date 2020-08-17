18ec4fc95e93fa21fec5ce927ba230a437ea4ba3
Petr Viktorin <encukou@gmail.com>
2020-08-17 19:04:50 +0200

    Add test_flask_url_for_custom_prefix

--- test_demo.py
+++ test_demo.py
@@ -139,6 +139,25 @@ def test_flask_url_for(tmp_path):
     assert path3.exists()
 
 
+def test_flask_url_for_custom_prefix(tmp_path):
+    """Test an app unsing Flask url_for() with custom host & path
+    """
+
+    freeze(app_url_for, tmp_path, prefix='http://freezeyt.test:1234/foo/')
+
+    with open(tmp_path / "index.html", encoding='utf-8') as f:
+        read_text = f.read()
+
+    assert 'http://freezeyt.test:1234/foo/second_page.html' in read_text
+    assert '/foo/third_page.html' in read_text
+
+    path2 = tmp_path / "second_page.html"
+    assert path2.exists()
+
+    path3 = tmp_path / "third_page.html"
+    assert path3.exists()
+
+
 def test_structured(tmp_path):
     """Test if an app with relative URLs works.
     """
