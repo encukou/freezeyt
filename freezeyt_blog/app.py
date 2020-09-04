46f667af5b76367bc47e39307d5367ac64b111ef
David Stroch <d.stroch@gmail.com>
2020-09-04 17:45:43 +0200

    Add completed app.py and rename files of articles

--- freezeyt_blog/app.py
+++ freezeyt_blog/app.py
@@ -1,28 +1,54 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
+from mistune import markdown
+from pathlib import Path
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     """Start page with list of articles."""
 
     return f"""
     <html>
         <head>
-            <title>Vítejte na blogu o projektu freezeyt !</title>
+            <title>Freezeyt blog</title>
         </head>
         <body>
-            Seznam článků k projektu:
+            <h1>Vítejte na blogu o projektu freezeyt !</h1>
+            <hr>
             <br>
-            {url_for()}
-            <a href='/second_page.html'>LINK</a> to second page.
+            Všechny články k projektu:
+            <br>
+            <br>
+            <a href={url_for('lesson6')}>Článek o lekci 6</a>
+            <br>
+            <a href={url_for('lesson7')}>Článek o lekci 7</a>
         </body>
 
     </html>
     """
 
-@app.route('/lesson6')
+@app.route('/lekce6')
 def lesson6():
+    md_file = Path('content/articles/lekce6.md')
+    with open(md_file, mode='r', encoding='UTF-8') as f:
+        md_content = f.read()
+
+    html_content = markdown(md_content)
+
+    return html_content
+
+@app.route('/lekce7')
+def lesson7():
+    md_file = Path('content/articles/lekce7.md')
+    with open(md_file, mode='r', encoding='UTF-8') as f:
+        md_content = f.read()
+
+    html_content = markdown(md_content)
+
+    return html_content
+
+
 
 
 
