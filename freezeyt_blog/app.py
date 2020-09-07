6950a71393b7cc0c88b43911f7fdc8183229c833
Petr Viktorin <encukou@gmail.com>
2020-09-07 18:48:53 +0200

    blog: Use a generic route for blog posts

--- freezeyt_blog/app.py
+++ freezeyt_blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, abort
 from mistune import markdown
 from pathlib import Path
 
@@ -22,29 +22,24 @@ def index():
             Všechny články k projektu:
             <br>
             <br>
-            <a href={url_for('lesson6')}>Článek o lekci 6</a>
+            <a href={url_for('post', slug='lekce6')}>Článek o lekci 6</a>
             <br>
-            <a href={url_for('lesson7')}>Článek o lekci 7</a>
+            <a href={url_for('post', slug='lekce7')}>Článek o lekci 7</a>
         </body>
 
     </html>
     """
 
-@app.route('/lekce6')
-def lesson6():
-    md_file = base_path / 'content/articles/lekce6.md'
-    with open(md_file, mode='r', encoding='UTF-8') as f:
-        md_content = f.read()
+@app.route('/<slug>')
+def post(slug):
+    md_file = base_path / f'content/articles/{slug}.md'
+    try:
+        file = open(md_file, mode='r', encoding='UTF-8')
+    except FileNotFoundError:
+        return abort(404)
 
-    html_content = markdown(md_content)
-
-    return html_content
-
-@app.route('/lekce7')
-def lesson7():
-    md_file = base_path / 'content/articles/lekce7.md'
-    with open(md_file, mode='r', encoding='UTF-8') as f:
-        md_content = f.read()
+    with file:
+        md_content = file.read()
 
     html_content = markdown(md_content)
 
