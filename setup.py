e6de8842aba07eb418c206be0000ab4c585efc55
Petr Viktorin <encukou@gmail.com>
2020-09-07 20:41:48 +0200

    Add setup.py and configuration for tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='freezeyt',
+    version='0.1',
+    description='...',
+    license='MIT',
+    url='https://github.com/encukou/freezeyt',
+    packages=['freezeyt'],
+)
