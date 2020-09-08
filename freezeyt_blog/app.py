9195ecbf8b5ecaa326bd425499066922afb570a7
David Stroch <d.stroch@gmail.com>
2020-09-08 11:28:58 +0200

    Generate list of articles at home page

--- freezeyt_blog/app.py
+++ freezeyt_blog/app.py
@@ -5,22 +5,27 @@ from pathlib import Path
 app = Flask(__name__)
 
 base_path = Path(__file__).parent
+content_path = base_path / 'content/'
+
+ARTICLES_PATH = content_path / 'articles/'
+STATIC_PATH = content_path / 'images/'
 
 @app.route('/')
 def index():
     """Start page with list of articles."""
+    post_names = [art_file.stem for art_file in ARTICLES_PATH.iterdir()]
 
     return render_template(
         'index.html',
-        post_names=['lekce6', 'lekce7'],
+        post_names=post_names,
     )
 
 
 @app.route('/<slug>')
 def post(slug):
-    md_file = base_path / f'content/articles/{slug}.md'
+    article = ARTICLES_PATH / f'{slug}.md'
     try:
-        file = open(md_file, mode='r', encoding='UTF-8')
+        file = open(article, mode='r', encoding='UTF-8')
     except FileNotFoundError:
         return abort(404)
 
