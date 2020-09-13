a39a8345d2a76388dc6fc8315be0d8c6f3635ff6
David Stroch <d.stroch@gmail.com>
2020-09-13 22:40:07 +0200

    Add highlight syntax of python code in html with pygments css

--- freezeyt_blog/app.py
+++ freezeyt_blog/app.py
@@ -1,6 +1,10 @@
+import mistune
 from flask import Flask, url_for, abort, render_template
-from mistune import markdown
 from pathlib import Path
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
 
 app = Flask(__name__)
 
@@ -9,6 +13,16 @@ content_path = base_path / 'content/'
 
 ARTICLES_PATH = content_path / 'articles/'
 STATIC_PATH = content_path / 'images/'
+
+class MyRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % \
+                mistune.escape(code)
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter()
+        return highlight(code, lexer, formatter)
+
 
 @app.route('/')
 def index():
@@ -32,7 +46,9 @@ def post(slug):
     with file:
         md_content = file.read()
 
-    html_content = markdown(md_content)
+    renderer = MyRenderer()
+    md = mistune.Markdown(renderer=renderer)
+    html_content = md.render(md_content)
 
     return render_template(
         'post.html',
