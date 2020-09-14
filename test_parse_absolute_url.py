5dc98df68c379401cf48c29abeb03951e23810a6
Marija Petkovska <mpetkovska@pixel.localdomain>
2020-09-14 17:15:32 +0200

    resolves issue#45 regarding test_parse_absolute_url

--- test_parse_absolute_url.py
+++ test_parse_absolute_url.py
@@ -12,6 +12,10 @@ def test_no_scheme():
     with pytest.raises(ValueError):
         parse_absolute_url("pyladies.cz/")
 
+def test_no_netloc():
+    with pytest.raises(ValueError):
+        parse_absolute_url("pyladies.cz/foo")
+
 def test_url_scheme():
     with pytest.raises(ValueError):
         parse_absolute_url("ftp://localhost:8000/second_page.html")
@@ -23,3 +27,11 @@ def test_no_port_http():
 def test_no_port_https():
     parsed = parse_absolute_url("https://naucse.python.cz/")
     assert parsed.netloc == 'naucse.python.cz:443'
+
+def test_port():
+    parsed = parse_absolute_url("http://freezeyt.test:1234/foo/")
+    assert parsed.port == 1234
+
+def test_netloc():
+    parsed = parse_absolute_url("http://freezeyt.test:1234/foo/")
+    assert parsed.netloc == parsed.hostname + ':1234'
