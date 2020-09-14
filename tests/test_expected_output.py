71dbdfef330304822feddbc5b1ededa83092b79a
Petr Viktorin <encukou@gmail.com>
2020-09-14 20:32:50 +0200

    Add test for files with the same stat signature

--- tests/test_expected_output.py
+++ tests/test_expected_output.py
@@ -84,3 +84,20 @@ def test_assert_dirs_same(dir_name):
     else:
         with pytest.raises(AssertionError):
             assert_dirs_same(path, DIRS_SAME_FIXTURES / 'testdir')
+
+
+def test_files_with_same_signature(tmp_path):
+    dir1 = tmp_path / 'dir1'
+    dir2 = tmp_path / 'dir2'
+
+    dir1.mkdir()
+    dir2.mkdir()
+
+    path1 = dir1 / 'file.txt'
+    path2 = dir2 / 'file.txt'
+
+    path1.write_text('A')
+    path2.write_text('B')
+
+    with pytest.raises(AssertionError):
+        assert_dirs_same(dir1, dir2)
