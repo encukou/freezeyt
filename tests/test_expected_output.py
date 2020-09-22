76a4508e7584b69b0b85e0dc097ca31a13dd45b0
David Stroch <d.stroch@gmail.com>
2020-09-22 15:08:28 +0200

    Update test_expected_output.py
    - Remove module name which is still same
    - Add block try finally to delete cache of imported module
    - Add condition for special case of demo app with broken link

--- tests/test_expected_output.py
+++ tests/test_expected_output.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import filecmp
 import os
 import shutil
+import sys
 
 import pytest
 
@@ -17,37 +18,40 @@ APP_NAMES = [p.name for p in FIXTURES_PATH.iterdir() if p.is_dir() and p != DIRS
 
 
 @pytest.mark.parametrize('app_name', APP_NAMES)
-def test_output(tmp_path, monkeypatch, app_name, module_name='app'):
+def test_output(tmp_path, monkeypatch, app_name):
     app_path = FIXTURES_PATH / app_name
-    module_path = app_path / str(module_name + '.py')
+    module_path = app_path / 'app.py'
 
     # Add FIXTURES_PATH to sys.path, the list of directories that `import`
     # looks in
     monkeypatch.syspath_prepend(str(app_path))
+    try:
+        module = importlib.import_module('app')
+        app = module.app
 
-    module = importlib.import_module(module_name)
-    app = module.app
-
-    expected = app_path / 'test_expected_output'
-    print("expected_path:", expected)
+        expected = app_path / 'test_expected_output'
+        print("expected_path:", expected)
 
 
-    if not module_path.exists():
-        with pytest.raises(ValueError):
+        if not module_path.exists() or app_name == 'demo_app_broken_link':
+            with pytest.raises(ValueError):
+                freeze(app, tmp_path)
+        else:
             freeze(app, tmp_path)
-    else:
-        freeze(app, tmp_path)
 
-        if not expected.exists():
-            if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
-                shutil.copytree(tmp_path, expected)
-            else:
-                raise AssertionError(
-                    f'Expected output directory ({expected}) does not exist. '
-                    + f'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
-                )
+            if not expected.exists():
+                if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
+                    shutil.copytree(tmp_path, expected)
+                else:
+                    raise AssertionError(
+                        f'Expected output directory ({expected}) does not exist. '
+                        + f'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
+                    )
 
-    assert_dirs_same(tmp_path, expected)
+            assert_dirs_same(tmp_path, expected)
+
+    finally:
+        sys.modules.pop('app', None)
 
 
 def assert_dirs_same(got: Path, expected: Path):
