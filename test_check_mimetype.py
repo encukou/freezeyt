c045b81ca393bee97ac173426f1d17d9b47c8e7c
David Stroch <d.stroch@gmail.com>
2020-09-30 14:06:43 +0200

    Add test for case sensitive and missing content type

--- test_check_mimetype.py
+++ test_check_mimetype.py
@@ -11,11 +11,64 @@ def test_normal_html():
         ]
     )
 
-def test_png_fail():
+def test_different_type_jpg_png_fail():
     with pytest.raises(ValueError):
         check_mimetype(
             '/tmp/image.jpg',
             [
-                ('Content-Type', 'image/png'), ('Content-Length', '654'),
+                ('Content-Type', 'image/png'),
+                ('Content-Length', '654'),
+            ],
+        )
+
+
+def test_case_sensitive():
+        check_mimetype(
+            '/tmp/index.html',
+            [
+                ('Content-Type', 'TEXT/HTML; charset=utf-8'),
+                ('Content-Length', '164'),
+            ]
+        )
+
+
+def test_missing_content_type():
+    with pytest.raises(ValueError):
+        check_mimetype(
+            '/tmp/index.html',
+            [
+                ('Content-Length', '164'),
+            ]
+        )
+
+
+def test_case_insensitive_fail():
+    with pytest.raises(ValueError):
+        check_mimetype(
+            '/tmp/image.jpg',
+            [
+                ('CONTENT-TYPE', 'IMAGE/PNG'),
+                ('Content-Length', '654'),
+            ],
+        )
+
+
+def test_case_insensitive_crazy():
+    check_mimetype(
+        '/tmp/image.jpg',
+        [
+            ('coNtENT-TypE', 'IMAgE/JpEG'),
+            ('Content-Length', '654'),
+        ],
+    )
+
+
+def test_same_jpg_fail():
+    with pytest.raises(ValueError):
+        check_mimetype(
+            '/tmp/image.jpg',
+            [
+                ('Content-Type', 'image/jpg'),
+                ('Content-Length', '654'),
             ],
         )
