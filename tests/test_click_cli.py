c9fad34a91ce0730377e606ba758e8f863bf18eb
David Stroch <d.stroch@gmail.com>
2020-11-06 13:02:01 +0100

    Fix problem with path to app

--- tests/test_click_cli.py
+++ tests/test_click_cli.py
@@ -1,3 +1,7 @@
+import importlib
+import pytest
+import sys
+
 from click.testing import CliRunner
 from freezeyt import cli
 from test_expected_output import APP_NAMES, FIXTURES_PATH, assert_dirs_same
@@ -10,36 +14,45 @@ def test_cli_output(tmp_path, monkeypatch, app_name):
     app_path = FIXTURES_PATH / app_name
     error_path = app_path / 'error.txt'
 
-    # I dont know how to handle this convert
-    app = "path/to/app as path.to.module"
 
-    cli_args = [app, tmp_path]
+    # Add FIXTURES_PATH to sys.path, the list of directories that `import`
+    # looks in
+    monkeypatch.syspath_prepend(str(app_path))
+    try:
+        module = importlib.import_module('app')
+        # app = module.app
 
-    for arg_name in 'prefix', 'extra_pages':
-        arg_value = getattr(module, arg_name, None)
-        if arg_value != None:
-            if arg_name == 'extra_pages':
-                make_cli_arg = lambda s: f"--extra-page {s}"
-                extra_pages = list(map(make_cli_arg, arg_value))
-                cli_args.extend(extra_pages)
-            else:
-                cli_args.append = arg_value
+        cli_args = [module, tmp_path]
+
+        for arg_name in 'prefix', 'extra_pages':
+            arg_value = getattr(module, arg_name, None)
+            if arg_value != None:
+                if arg_name == 'extra_pages':
+                    make_cli_arg = lambda s: f"--extra-page {s}"
+                    extra_pages = list(map(make_cli_arg, arg_value))
+                    cli_args.extend(extra_pages)
+                else:
+                    cli_args.append = arg_value
 
 
-    expected = app_path / 'test_expected_output'
+        expected = app_path / 'test_expected_output'
 
-    if error_path.exists():
-        with pytest.raises(ValueError):
-            runner.invoke(cli, cli_args)
-    else:
-        runner.invoke(cli, cli_args)
+        if error_path.exists():
+            with pytest.raises(ValueError):
+                runner.invoke(cli, cli_args)
 
-        if not expected.exists():
-            raise AssertionError(
-                f'Expected output directory ({expected}) does not exist. '
-                + '\nRun $ python -m pytest test_expected_output.py\n'
-                + 'And follow instructions'
+        else:
+            result = runner.invoke(cli, cli_args)
+            print('Result:', result)
 
-                )
+            if not expected.exists():
+                raise AssertionError(
+                    f'Expected output directory ({expected}) does not exist. '
+                    + '\nRun $ python -m pytest test_expected_output.py\n'
+                    + 'And follow instructions'
 
-        assert_dirs_same(tmp_path, expected)
+                    )
+
+            assert_dirs_same(tmp_path, expected)
+    finally:
+        sys.modules.pop('app', None)
