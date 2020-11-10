9ad601ad6dadbb559d2708d0146c66632ead24d2
David Stroch <d.stroch@gmail.com>
2020-11-10 16:21:31 +0100

    Add pytest.skip() for app with extra files. CLI can not handle at this moment

--- tests/test_click_cli.py
+++ tests/test_click_cli.py
@@ -28,7 +28,7 @@ def test_cli_output(tmp_path, monkeypatch, app_name):
         print('MODULE:', module)
         cli_args = [str(module_path), str(tmp_path)]
 
-        for arg_name in 'prefix', 'extra_pages':
+        for arg_name in 'prefix', 'extra_pages', 'extra_files':
             arg_value = getattr(module, arg_name, None)
             if arg_value != None:
                 if arg_name == 'prefix':
@@ -36,6 +36,8 @@ def test_cli_output(tmp_path, monkeypatch, app_name):
                 elif arg_name == 'extra_pages':
                     for page in arg_value:
                         cli_args.extend(['--extra-page', page])
+                elif arg_name == 'extra_files':
+                    pytest.skip("extra_files not supported in CLI")
                 else:
                     cli_args.append(arg_value)
 
