24f3bb72fd3edea19c1e31613059ea60311fe5f2
David Stroch <d.stroch@gmail.com>
2020-11-12 09:25:20 +0100

    Handle merging input data from options and config file at one moment.

--- freezeyt/cli.py
+++ freezeyt/cli.py
@@ -5,10 +5,13 @@ import yaml
 from freezeyt.freezing import freeze
 
 
-@click.command()
+DEFAULT_PREFIX = 'http://localhost:8000/'
+
+
+@click.command(context_settings={'show_default': True})
 @click.argument('module_name')
 @click.argument('dest_path')
-@click.option('--prefix', default='http://localhost:8000/', help='URL of the application root')
+@click.option('--prefix', default=DEFAULT_PREFIX, show_default=True, help='URL of the application root')
 @click.option('--extra-page', multiple=True, help='Pages without any link in application')
 @click.option('-c', '--config', default=None, type=click.File(), help='YAML file of configuration')
 def main(module_name, dest_path, prefix, extra_page, config):
@@ -23,17 +26,35 @@ def main(module_name, dest_path, prefix, extra_page, config):
     Example use:
         python -m freezeyt demo_app build
     """
-    if config != None:
-        config_cont = yaml.safe_load(config)
-        if not isinstance(config_cont, dict):
-            raise SyntaxError(f'File {config.name} is not made in a YAML convence')
-        else:
-            print("Load configuration was succesful")
-    else:
-        config_cont = None
+    cli_params = {
+        'prefix': prefix,
+        'extra_pages': list(extra_page),
+        'extra_files': None,
+    }
 
+    if config:
+        file_config = yaml.safe_load(config)
+
+        if not isinstance(file_config, dict):
+            raise SyntaxError(
+                    f'File {config.name} is not prepared as YAML dictionary.'
+                    )
+        else:
+            print("Loading config YAML file was successful")
+
+            if file_config.get('prefix', None) != None:
+                if prefix == DEFAULT_PREFIX:
+                    cli_params['prefix'] = file_config['prefix']
+                else:
+                    print("WARNING: Conflicting input data in prefix!")
+                    print(f"Prefix is setup to {prefix}")
+
+            if file_config.get('extra_pages', None) != None:
+                cli_params['extra_pages'].extend(file_config['extra_pages'])
+
+            cli_params['extra_files'] = file_config.get('extra_files', None)
 
     module = importlib.import_module(module_name)
     app = module.app
 
-    freeze(app, dest_path, prefix=prefix, extra_pages=extra_page, config=config_cont)
+    freeze(app, dest_path, **cli_params)
