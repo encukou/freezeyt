3198edff69bba9dc14b1eb26a344f68ec59ba7ff
Petr Viktorin <encukou@gmail.com>
2020-11-16 21:07:28 +0100

    Use BytesIO in tests

--- tests/test_get_links_css.py
+++ tests/test_get_links_css.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from pathlib import Path
 
 from freezeyt.freezing import get_links_from_css
@@ -5,7 +6,7 @@ from freezeyt.freezing import get_links_from_css
 def test_get_links_css():
     # css_file = Path(__file__).parent / 'fixtures' / 'css_files' / 'style.css'
     # with open(css_file, "rb") as f:
-    f = b"""
+    f = BytesIO(b"""
     @font-face {
             font-family: 'TurretRoad';
             src: url(TurretRoad-Regular.ttf) format('truetype');
@@ -15,6 +16,6 @@ def test_get_links_css():
         body {
             font-family: 'TurretRoad';
         }
-    """
+    """)
     links = get_links_from_css(f, 'http://localhost:8000')
     assert sorted(links) == ['http://localhost:8000/TurretRoad-Regular.ttf']
