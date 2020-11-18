7856c18be7498121fcec184744fef20af4152482
David Stroch <d.stroch@gmail.com>
2020-11-18 15:11:45 +0100

    Add test to check conflict between option --prefix and prefix load from config file

--- tests/test_click_cli.py
+++ tests/test_click_cli.py
@@ -12,7 +12,7 @@ from test_expected_output import APP_NAMES, FIXTURES_PATH, assert_dirs_same
 
 
 @pytest.mark.parametrize('app_name', APP_NAMES)
-def test_cli_with_fixtures_output(tmp_path, monkeypatch, app_name):
+def test_cli_with_fixtures_output(tmp_path, app_name):
     app_dir = Path('fixtures', app_name)
     expected = FIXTURES_PATH / app_name / 'test_expected_output'
     module_path = app_dir / 'app'
@@ -123,6 +123,46 @@ def test_cli_with_extra_page_option(tmp_path):
         assert result.exit_code == 0
 
         assert_dirs_same(tmp_path, expected)
+
+    finally:
+        sys.modules.pop('app', None)
+
+
+def test_cli_prefix_conflict(tmp_path):
+    module_path = Path('fixtures', 'demo_app_url_for_prefix', 'app')
+    module_path = '.'.join(module_path.parts)
+    expected = FIXTURES_PATH / 'demo_app_url_for_prefix' / 'test_expected_output'
+    config_file = tmp_path / 'config.yaml'
+    build_dir = tmp_path / 'build'
+    build_dir.mkdir()
+
+    runner = CliRunner()
+
+    try:
+        module = importlib.import_module(module_path)
+        freeze_config = getattr(module, 'freeze_config')
+        prefix = freeze_config['prefix']
+        cli_args = [str(module_path), str(build_dir), '--prefix', prefix]
+
+        data = {'prefix': 'http://pyladies.cz/lessons/'}
+        with open(config_file, mode='w') as file:
+            safe_dump(data, stream=file)
+
+        cli_args.extend(['--config', config_file])
+
+        result = runner.invoke(main, cli_args)
+
+        if not expected.exists():
+            if not 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
+                raise AssertionError(
+                    f'Expected output directory ({expected}) does not exist. '
+                    + '\nRun $ python -m pytest test_expected_output.py\n'
+                    + 'And follow instructions'
+                    )
+
+        assert result.exit_code == 0
+
+        assert_dirs_same(build_dir, expected)
 
     finally:
         sys.modules.pop('app', None)
