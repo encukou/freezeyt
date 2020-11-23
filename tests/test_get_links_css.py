f1e88526881a73c7864c8ccc444d429cdef9c6bf
Adel Popelkova <adel.popelek@gmail.com>
2020-11-23 19:05:50 +0100

    Add tests for links in CSS

--- tests/test_get_links_css.py
+++ tests/test_get_links_css.py
@@ -17,3 +17,79 @@ def test_get_links_css():
     """)
     links = get_links_from_css(f, 'http://localhost:8000')
     assert sorted(links) == ['http://localhost:8000/TurretRoad-Regular.ttf']
+
+
+def test_links_css_two_links():
+    f = BytesIO(b"""
+        @font-face {
+            font-family: 'TurretRoad';
+            src: url(TurretRoad-Regular.ttf) format('truetype');
+            font-weight: normal;
+        }
+
+        body {
+            font-family: 'TurretRoad';
+            background-image: url('some-image.png');
+        }
+    """)
+    links = get_links_from_css(f, 'http://localhost:8000')
+    assert sorted(links) == [
+        'http://localhost:8000/TurretRoad-Regular.ttf',
+        'http://localhost:8000/some-image.png'
+    ]
+
+
+def test_links_css_without_links():
+    f = BytesIO(b"""
+        body {
+            background-color: #C2F5FF;
+        }
+    """)
+    links = get_links_from_css(f, 'http://localhost:8000')
+    assert links == []
+
+
+def test_links_css_static():
+    f = BytesIO(b"""
+        @import url('static/style.css');
+        body {
+            background-color: #C2F5FF;
+        }
+    """)
+    links = get_links_from_css(f, 'http://localhost:8000')
+    assert sorted(links) == ['http://localhost:8000/static/style.css']
+
+
+def test_links_css_one_external():
+    f = BytesIO(b"""
+        @font-face {
+            font-family: 'TurretRoad';
+            src: url(TurretRoad-Regular.ttf) format('truetype');
+            font-weight: normal;
+        }
+
+        body {
+            font-family: 'TurretRoad';
+            background-image: url('https://www.example.com/some-image.png');
+        }
+    """)
+    links = get_links_from_css(f, 'http://localhost:8000')
+    assert sorted(links) == [
+        'http://localhost:8000/TurretRoad-Regular.ttf',
+        'https://www.example.com/some-image.png'
+    ]
+
+
+def test_links_css_all_external():
+    f = BytesIO(b"""
+        @import url('https://www.example.com/style.css');
+
+        body {
+            background-image: url('https://www.example.com/some-image.png');
+        }
+    """)
+    links = get_links_from_css(f, 'http://localhost:8000')
+    assert sorted(links) == [
+        'https://www.example.com/some-image.png',
+        'https://www.example.com/style.css'
+    ]
