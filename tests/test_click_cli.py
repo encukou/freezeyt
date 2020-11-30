8dd127f84f0e1827bc18b215534b2f5807c90e3c
Petr Viktorin <encukou@gmail.com>
2020-11-30 21:42:34 +0100

    Move some common code into a function

--- tests/test_click_cli.py
+++ tests/test_click_cli.py
@@ -11,15 +11,36 @@ from freezeyt.cli import main
 from test_expected_output import APP_NAMES, FIXTURES_PATH, assert_dirs_same
 
 
+def run_and_check(runner, cli_args, app_name, build_dir):
+    error_path = FIXTURES_PATH / app_name / 'error.txt'
+    expected = FIXTURES_PATH / app_name / 'test_expected_output'
+
+    build_dir.mkdir()
+
+    result = runner.invoke(main, cli_args)
+    if error_path.exists():
+        assert result.exit_code != 0
+
+    else:
+        if not expected.exists():
+            if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
+                pytest.skip('Expected output is created in other tests')
+            else:
+                raise AssertionError(
+                    f'Expected output directory ({expected}) does not exist. '
+                    + 'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
+                )
+        else:
+            assert result.exit_code == 0
+
+            assert_dirs_same(build_dir, expected)
+
+
 @pytest.mark.parametrize('app_name', APP_NAMES)
 def test_cli_with_fixtures_output(tmp_path, app_name, monkeypatch):
     app_dir = FIXTURES_PATH / app_name
-    expected = FIXTURES_PATH / app_name / 'test_expected_output'
-    error_path = FIXTURES_PATH / app_name / 'error.txt'
     config_file = tmp_path / 'config.yaml'
     build_dir = tmp_path / 'build'
-
-    build_dir.mkdir()
 
     runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
     monkeypatch.syspath_prepend(app_dir)
@@ -35,33 +56,16 @@ def test_cli_with_fixtures_output(tmp_path, app_name, monkeypatch):
 
             cli_args.extend(['--config', config_file])
 
-        result = runner.invoke(main, cli_args)
-        if error_path.exists():
-            assert result.exit_code != 0
-
-        else:
-            if not expected.exists():
-                if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
-                    pytest.skip('Expected output is created in other tests')
-                else:
-                    raise AssertionError(
-                        f'Expected output directory ({expected}) does not exist. '
-                        + 'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
-                    )
-            else:
-                assert result.exit_code == 0
-
-                assert_dirs_same(build_dir, expected)
+        run_and_check(runner, cli_args, app_name, build_dir)
 
     finally:
         sys.modules.pop('app', None)
 
 
 def test_cli_with_prefix_option(tmp_path, monkeypatch):
-    app_dir = FIXTURES_PATH / 'demo_app_url_for_prefix'
-    expected = FIXTURES_PATH / 'demo_app_url_for_prefix' / 'test_expected_output'
+    app_name = 'demo_app_url_for_prefix'
+    app_dir = FIXTURES_PATH / app_name
     build_dir = tmp_path / 'build'
-    build_dir.mkdir()
 
     runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
     monkeypatch.syspath_prepend(app_dir)
@@ -72,30 +76,16 @@ def test_cli_with_prefix_option(tmp_path, monkeypatch):
         prefix = freeze_config['prefix']
         cli_args = ['app', str(build_dir), '--prefix', prefix]
 
-        result = runner.invoke(main, cli_args)
-
-        if not expected.exists():
-            if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
-                pytest.skip('Expected output is created in other tests')
-            else:
-                raise AssertionError(
-                    f'Expected output directory ({expected}) does not exist. '
-                    + 'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
-                )
-        else:
-            assert result.exit_code == 0
-
-            assert_dirs_same(build_dir, expected)
+        run_and_check(runner, cli_args, app_name, build_dir)
 
     finally:
         sys.modules.pop('app', None)
 
 
 def test_cli_with_extra_page_option(tmp_path, monkeypatch):
-    app_dir = FIXTURES_PATH / 'app_with_extra_page_deep'
-    expected = FIXTURES_PATH / 'app_with_extra_page_deep' / 'test_expected_output'
+    app_name = 'app_with_extra_page_deep'
+    app_dir = FIXTURES_PATH / app_name
     build_dir = tmp_path / 'build'
-    build_dir.mkdir()
 
     runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
     monkeypatch.syspath_prepend(app_dir)
@@ -113,31 +103,17 @@ def test_cli_with_extra_page_option(tmp_path, monkeypatch):
 
         cli_args.extend(extra_pages)
 
-        result = runner.invoke(main, cli_args)
-
-        if not expected.exists():
-            if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
-                pytest.skip('Expected output is created in other tests')
-            else:
-                raise AssertionError(
-                    f'Expected output directory ({expected}) does not exist. '
-                    + 'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
-                )
-        else:
-            assert result.exit_code == 0
-
-            assert_dirs_same(build_dir, expected)
+        run_and_check(runner, cli_args, app_name, build_dir)
 
     finally:
         sys.modules.pop('app', None)
 
 
 def test_cli_prefix_conflict(tmp_path, monkeypatch):
-    app_dir = FIXTURES_PATH / 'demo_app_url_for_prefix'
-    expected = FIXTURES_PATH / 'demo_app_url_for_prefix' / 'test_expected_output'
+    app_name = 'demo_app_url_for_prefix'
+    app_dir = FIXTURES_PATH / app_name
     config_file = tmp_path / 'config.yaml'
     build_dir = tmp_path / 'build'
-    build_dir.mkdir()
 
     runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
     monkeypatch.syspath_prepend(app_dir)
@@ -154,20 +130,7 @@ def test_cli_prefix_conflict(tmp_path, monkeypatch):
 
         cli_args.extend(['--config', config_file])
 
-        result = runner.invoke(main, cli_args)
-
-        if not expected.exists():
-            if 'TEST_CREATE_EXPECTED_OUTPUT' in os.environ:
-                pytest.skip('Expected output is created in other tests')
-            else:
-                raise AssertionError(
-                    f'Expected output directory ({expected}) does not exist. '
-                    + 'Run with TEST_CREATE_EXPECTED_OUTPUT=1 to create it'
-                )
-        else:
-            assert result.exit_code == 0
-
-            assert_dirs_same(build_dir, expected)
+        run_and_check(runner, cli_args, app_name, build_dir)
 
     finally:
         sys.modules.pop('app', None)
