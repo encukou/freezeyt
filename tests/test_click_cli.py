edad49aee34f377f0501b0b11048689c57e79dbb
Petr Viktorin <encukou@gmail.com>
2020-11-30 22:00:50 +0100

    Move common code to a context manager

--- tests/test_click_cli.py
+++ tests/test_click_cli.py
@@ -2,6 +2,7 @@ import importlib
 import os
 import sys
 from pathlib import Path
+from contextlib import contextmanager
 
 import pytest
 from yaml import safe_dump
@@ -11,11 +12,14 @@ from freezeyt.cli import main
 from test_expected_output import APP_NAMES, FIXTURES_PATH, assert_dirs_same
 
 
-def run_and_check(runner, cli_args, app_name, build_dir):
+def run_and_check(cli_args, app_name, build_dir):
+    app_dir = FIXTURES_PATH / app_name
     error_path = FIXTURES_PATH / app_name / 'error.txt'
     expected = FIXTURES_PATH / app_name / 'test_expected_output'
 
     build_dir.mkdir()
+
+    runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
 
     result = runner.invoke(main, cli_args)
     if error_path.exists():
@@ -36,17 +40,24 @@ def run_and_check(runner, cli_args, app_name, build_dir):
             assert_dirs_same(build_dir, expected)
 
 
+@contextmanager
+def context_for_test(app_name, monkeypatch):
+    app_dir = FIXTURES_PATH / app_name
+    monkeypatch.syspath_prepend(app_dir)
+    try:
+        module = importlib.import_module('app')
+        yield module
+    finally:
+        sys.modules.pop('app', None)
+
+
 @pytest.mark.parametrize('app_name', APP_NAMES)
 def test_cli_with_fixtures_output(tmp_path, app_name, monkeypatch):
     app_dir = FIXTURES_PATH / app_name
     config_file = tmp_path / 'config.yaml'
     build_dir = tmp_path / 'build'
 
-    runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
-    monkeypatch.syspath_prepend(app_dir)
-
-    try:
-        module = importlib.import_module('app')
+    with context_for_test(app_name, monkeypatch) as module:
         cli_args = ['app', str(build_dir)]
         freeze_config = getattr(module, 'freeze_config', None)
 
@@ -56,10 +67,7 @@ def test_cli_with_fixtures_output(tmp_path, app_name, monkeypatch):
 
             cli_args.extend(['--config', config_file])
 
-        run_and_check(runner, cli_args, app_name, build_dir)
-
-    finally:
-        sys.modules.pop('app', None)
+        run_and_check(cli_args, app_name, build_dir)
 
 
 def test_cli_with_prefix_option(tmp_path, monkeypatch):
@@ -67,19 +75,12 @@ def test_cli_with_prefix_option(tmp_path, monkeypatch):
     app_dir = FIXTURES_PATH / app_name
     build_dir = tmp_path / 'build'
 
-    runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
-    monkeypatch.syspath_prepend(app_dir)
-
-    try:
-        module = importlib.import_module('app')
+    with context_for_test(app_name, monkeypatch) as module:
         freeze_config = getattr(module, 'freeze_config')
         prefix = freeze_config['prefix']
         cli_args = ['app', str(build_dir), '--prefix', prefix]
 
-        run_and_check(runner, cli_args, app_name, build_dir)
-
-    finally:
-        sys.modules.pop('app', None)
+        run_and_check(cli_args, app_name, build_dir)
 
 
 def test_cli_with_extra_page_option(tmp_path, monkeypatch):
@@ -87,11 +88,7 @@ def test_cli_with_extra_page_option(tmp_path, monkeypatch):
     app_dir = FIXTURES_PATH / app_name
     build_dir = tmp_path / 'build'
 
-    runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
-    monkeypatch.syspath_prepend(app_dir)
-
-    try:
-        module = importlib.import_module('app')
+    with context_for_test(app_name, monkeypatch) as module:
         cli_args = ['app', str(build_dir)]
 
         freeze_config = getattr(module, 'freeze_config')
@@ -103,10 +100,7 @@ def test_cli_with_extra_page_option(tmp_path, monkeypatch):
 
         cli_args.extend(extra_pages)
 
-        run_and_check(runner, cli_args, app_name, build_dir)
-
-    finally:
-        sys.modules.pop('app', None)
+        run_and_check(cli_args, app_name, build_dir)
 
 
 def test_cli_prefix_conflict(tmp_path, monkeypatch):
@@ -115,11 +109,7 @@ def test_cli_prefix_conflict(tmp_path, monkeypatch):
     config_file = tmp_path / 'config.yaml'
     build_dir = tmp_path / 'build'
 
-    runner = CliRunner(env={'PYTHONPATH': str(app_dir)})
-    monkeypatch.syspath_prepend(app_dir)
-
-    try:
-        module = importlib.import_module('app')
+    with context_for_test(app_name, monkeypatch) as module:
         freeze_config = getattr(module, 'freeze_config')
         prefix = freeze_config['prefix']
         cli_args = ['app', str(build_dir), '--prefix', prefix]
@@ -130,7 +120,4 @@ def test_cli_prefix_conflict(tmp_path, monkeypatch):
 
         cli_args.extend(['--config', config_file])
 
-        run_and_check(runner, cli_args, app_name, build_dir)
-
-    finally:
-        sys.modules.pop('app', None)
+        run_and_check(cli_args, app_name, build_dir)
