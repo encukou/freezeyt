b5ab275b0365e7d7d7e3c1389779bc0c6a3e04c3
Adel Popelkova <adel.popelek@gmail.com>
2021-03-09 18:22:54 +0100

    Switch to css-parser from cssutils

--- freezeyt/getlinks_css.py
+++ freezeyt/getlinks_css.py
@@ -1,14 +1,14 @@
 from urllib.parse import urljoin
 
-import cssutils
+import css_parser
 
 
 def get_links_from_css(css_file, base_url):
     """Get all links from a CSS file."""
     result = []
     text = css_file.read()
-    parsed = cssutils.parseString(text)
-    all_urls = cssutils.getUrls(parsed)
+    parsed = css_parser.parseString(text)
+    all_urls = css_parser.getUrls(parsed)
     for url in all_urls:
         result.append(urljoin(base_url, url))
     return result
