130db7db27712a58a7a8f5a20896092e62bc8a3f
Petr Viktorin <encukou@gmail.com>
2021-04-06 18:39:09 +0200

    Simplify logic of copying YAML config to dict
    Load the config from YAML, then add values
    specified in the CLI.

--- freezeyt/cli.py
+++ freezeyt/cli.py
@@ -9,9 +9,11 @@ from freezeyt.util import import_variable_from_module
 @click.argument('module_name')
 @click.argument('dest_path', required=False)
 @click.option('--prefix', help='URL of the application root')
-@click.option('--extra-page', multiple=True, help='Pages without any link in application')
-@click.option('-c', '--config', type=click.File(), help='YAML file of configuration')
-def main(module_name, dest_path, prefix, extra_page, config):
+@click.option('--extra-page', 'extra_pages', multiple=True,
+              help='Pages without any link in application')
+@click.option('-c', '--config', 'config_file', type=click.File(),
+              help='YAML file of configuration')
+def main(module_name, dest_path, prefix, extra_pages, config_file):
     """
     MODULE_NAME
         Name of the Python web app module which will be frozen.
@@ -34,34 +36,22 @@ def main(module_name, dest_path, prefix, extra_page, config):
 
         python -m freezeyt demo_app build -c config.yaml
     """
-    cli_params = {
-        'extra_pages': list(extra_page)
-    }
+    if config_file != None:
+        config = yaml.safe_load(config_file)
+        if not isinstance(config, dict):
+            raise SyntaxError(
+                    f'File {config_file.name} is not a YAML dictionary.'
+                    )
+    else:
+        config = {}
+
+    if extra_pages:
+        config.setdefault('extra_pages', []).extend(extra_pages)
 
     if prefix != None:
-        cli_params['prefix'] = prefix
+        config['prefix'] = prefix
 
-    if config != None:
-        file_config = yaml.safe_load(config)
-
-        if not isinstance(file_config, dict):
-            raise SyntaxError(
-                    f'File {config.name} is not prepared as YAML dictionary.'
-                    )
-        else:
-            print("Loading config YAML file was successful")
-
-            if (file_config.get('prefix', None) != None) and (prefix is None):
-                cli_params['prefix'] = file_config['prefix']
-
-            cli_params['extra_pages'].extend(file_config.get('extra_pages', []))
-
-            cli_params['extra_files'] = file_config.get('extra_files', None)
-
-        if 'output' in file_config:
-            cli_params['output'] = file_config['output']
-
-    if 'output' in cli_params:
+    if 'output' in config:
         if dest_path is not None:
             raise click.UsageError(
                 'DEST_PATH argument is not needed if output is configured from file'
@@ -69,10 +59,10 @@ def main(module_name, dest_path, prefix, extra_page, config):
     else:
         if dest_path is None:
             raise click.UsageError('DEST_PATH argument is required')
-        cli_params['output'] = {'type': 'dir', 'dir': dest_path}
+        config['output'] = {'type': 'dir', 'dir': dest_path}
 
     app = import_variable_from_module(
         module_name, default_variable_name='app',
     )
 
-    freeze(app, cli_params)
+    freeze(app, config)
