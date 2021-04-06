37f4d255659ce0e4cd5458186d903e60259be79a
Petr Viktorin <encukou@gmail.com>
2021-04-06 19:59:55 +0200

    Parametrize test_url_to_path

--- tests/test_url_to_path.py
+++ tests/test_url_to_path.py
@@ -6,85 +6,87 @@ import pytest
 from freezeyt.freezer import url_to_path
 
 
-def test_index():
-    prefix = url_parse('http://localhost:8000/')
-    result = url_to_path(prefix, url_parse("http://localhost:8000/"))
-    assert result == PurePosixPath("index.html")
+POSITIVE_TEST_CASES = {
+    'index': (
+        'http://localhost:8000/',
+        "http://localhost:8000/",
+        "index.html",
+    ),
+    'second_page': (
+        'http://localhost:8000/',
+        "http://localhost:8000/second/",
+        "second/index.html",
+    ),
+    'second_page_html': (
+        'http://localhost:8000/',
+        "http://localhost:8000/second.html",
+        "second.html",
+    ),
+    'fragment': (
+        'http://localhost:8000/',
+        "http://localhost:8000/second_page.html#odkaz",
+        "second_page.html",
+    ),
+    'custom_prefix_index': (
+        'http://freezeyt.test:1234/foo/',
+        "http://freezeyt.test:1234/foo/",
+        'index.html',
+    ),
+    'custom_prefix_second_page': (
+        'http://freezeyt.test:1234/foo/',
+        "http://freezeyt.test:1234/foo/second/",
+        "second/index.html",
+    ),
+    'custom_prefix_second_page_html': (
+        'http://freezeyt.test:1234/foo/',
+        "http://freezeyt.test:1234/foo/second.html",
+        "second.html",
+    ),
+    'custom_prefix_fragment': (
+        'http://freezeyt.test:1234/foo/',
+        "http://freezeyt.test:1234/foo/second_page.html#odkaz",
+        "second_page.html",
+    ),
+}
+
+@pytest.mark.parametrize('case', POSITIVE_TEST_CASES)
+def test_positive(case):
+    prefix_string, url_string, expected = POSITIVE_TEST_CASES[case]
+    prefix = url_parse(prefix_string)
+    result = url_to_path(prefix, url_parse(url_string))
+    assert result == PurePosixPath(expected)
 
 
-def test_second_page():
-    prefix = url_parse('http://localhost:8000/')
-    result = url_to_path(prefix, url_parse("http://localhost:8000/second/"))
-    assert result == PurePosixPath("second/index.html")
+NEGATIVE_TEST_CASES = {
+    'external_page': (
+        'http://localhost:8000/',
+        "http://python.cz",
+    ),
+    'scheme_different_ftp': (
+        'http://localhost:8000/',
+        "ftp://localhost:8000/second_page.html",
+    ),
+    'scheme_different': (
+        'http://localhost:8000/',
+        "https://localhost:8000/second_page.html",
+    ),
+    'relative': (
+        'http://localhost:8000/',
+        "/a/b/c",
+    ),
+    'custom_prefix_fragment_ftp': (
+        'http://freezeyt.test:1234/foo/',
+        "ftp://localhost:8000/second_page.html",
+    ),
+    'custom_prefix_above': (
+        'http://freezeyt.test:1234/foo/',
+        "http://freezeyt.test:1234/second_page.html",
+    ),
+}
 
-
-def test_second_page_html():
-    prefix = url_parse('http://localhost:8000/')
-    result = url_to_path(prefix,url_parse("http://localhost:8000/second.html"))
-    assert result == PurePosixPath("second.html")
-
-
-def test_external_page():
-    prefix = url_parse('http://localhost:8000/')
+@pytest.mark.parametrize('case', NEGATIVE_TEST_CASES)
+def test_negative(case):
+    prefix_string, url_string = NEGATIVE_TEST_CASES[case]
+    prefix = url_parse(prefix_string)
     with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("http://python.cz"))
-
-
-def test_fragment():
-    prefix = url_parse('http://localhost:8000/')
-    result = url_to_path(prefix, url_parse("http://localhost:8000/second_page.html#odkaz"))
-    assert result == PurePosixPath("second_page.html")
-
-
-def test_scheme_different_ftp():
-    prefix = url_parse('http://localhost:8000/')
-    with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("ftp://localhost:8000/second_page.html"))
-
-
-def test_scheme_different():
-    prefix = url_parse('http://localhost:8000/')
-    with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("https://localhost:8000/second_page.html"))
-
-
-def test_relative():
-    prefix = url_parse('http://localhost:8000/')
-    with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("/a/b/c"))
-
-
-def test_custom_prefix_index():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    result = url_to_path(prefix, url_parse("http://freezeyt.test:1234/foo/"))
-    assert result == PurePosixPath('index.html')
-
-
-def test_custom_prefix_second_page():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    result = url_to_path(prefix, url_parse("http://freezeyt.test:1234/foo/second/"))
-    assert result == PurePosixPath("second/index.html")
-
-
-def test_custom_prefix_second_page_html():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    result = url_to_path(prefix, url_parse("http://freezeyt.test:1234/foo/second.html"))
-    assert result == PurePosixPath("second.html")
-
-
-def test_custom_prefix_fragment():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    result = url_to_path(prefix, url_parse("http://freezeyt.test:1234/foo/second_page.html#odkaz"))
-    assert result == PurePosixPath("second_page.html")
-
-
-def test_custom_prefix_fragment_ftp():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("ftp://localhost:8000/second_page.html"))
-
-
-def test_custom_prefix_above():
-    prefix = url_parse('http://freezeyt.test:1234/foo/')
-    with pytest.raises(ValueError):
-        url_to_path(prefix, url_parse("http://freezeyt.test:1234/second_page.html"))
+        url_to_path(prefix, url_parse(url_string))
