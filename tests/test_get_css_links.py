a7dd562078c72fe4b5699ef4f97aede14c4d3ae3
Petr Viktorin <encukou@gmail.com>
2021-06-29 18:51:51 +0200

    Parametrize test_get_css_links

--- tests/test_get_css_links.py
+++ tests/test_get_css_links.py
@@ -1,95 +1,95 @@
 from io import BytesIO
 
+import pytest
+
 from freezeyt.getlinks_css import get_links_from_css
 
+TEST_DATA = {
+    "basic": (
+        b"""
+            @font-face {
+                font-family: 'TurretRoad';
+                src: url(TurretRoad-Regular.ttf) format('truetype');
+                font-weight: normal;
+            }
 
-def test_get_links_css():
-    f = BytesIO(b"""
-        @font-face {
-            font-family: 'TurretRoad';
-            src: url(TurretRoad-Regular.ttf) format('truetype');
-            font-weight: normal;
-        }
+            body {
+                font-family: 'TurretRoad';
+            }
+        """,
+        ['http://localhost:8000/TurretRoad-Regular.ttf'],
+    ),
+    "two_links": (
+        b"""
+            @font-face {
+                font-family: 'TurretRoad';
+                src: url(TurretRoad-Regular.ttf) format('truetype');
+                font-weight: normal;
+            }
 
-        body {
-            font-family: 'TurretRoad';
-        }
-    """)
+            body {
+                font-family: 'TurretRoad';
+                background-image: url('some-image.png');
+            }
+        """,
+        [
+            'http://localhost:8000/TurretRoad-Regular.ttf',
+            'http://localhost:8000/some-image.png'
+        ],
+    ),
+    "without_links": (
+        b"""
+            body {
+                background-color: #C2F5FF;
+            }
+        """,
+        [],
+    ),
+    "static": (
+        b"""
+            @import url('static/style.css');
+            body {
+                background-color: #C2F5FF;
+            }
+        """,
+        ['http://localhost:8000/static/style.css']
+    ),
+    "one_external": (
+        b"""
+            @font-face {
+                font-family: 'TurretRoad';
+                src: url(TurretRoad-Regular.ttf) format('truetype');
+                font-weight: normal;
+            }
+
+            body {
+                font-family: 'TurretRoad';
+                background-image: url('https://www.example.com/some-image.png');
+            }
+        """,
+        [
+            'http://localhost:8000/TurretRoad-Regular.ttf',
+            'https://www.example.com/some-image.png'
+        ]
+    ),
+    "all_external": (
+        b"""
+            @import url('https://www.example.com/style.css');
+
+            body {
+                background-image: url('https://www.example.com/some-image.png');
+            }
+        """,
+        [
+            'https://www.example.com/some-image.png',
+            'https://www.example.com/style.css'
+        ],
+    ),
+}
+
+@pytest.mark.parametrize("test_name", TEST_DATA)
+def test_links_css(test_name):
+    input, expected = TEST_DATA[test_name]
+    f = BytesIO(input)
     links = get_links_from_css(f, 'http://localhost:8000')
-    assert sorted(links) == ['http://localhost:8000/TurretRoad-Regular.ttf']
-
-
-def test_links_css_two_links():
-    f = BytesIO(b"""
-        @font-face {
-            font-family: 'TurretRoad';
-            src: url(TurretRoad-Regular.ttf) format('truetype');
-            font-weight: normal;
-        }
-
-        body {
-            font-family: 'TurretRoad';
-            background-image: url('some-image.png');
-        }
-    """)
-    links = get_links_from_css(f, 'http://localhost:8000')
-    assert sorted(links) == [
-        'http://localhost:8000/TurretRoad-Regular.ttf',
-        'http://localhost:8000/some-image.png'
-    ]
-
-
-def test_links_css_without_links():
-    f = BytesIO(b"""
-        body {
-            background-color: #C2F5FF;
-        }
-    """)
-    links = get_links_from_css(f, 'http://localhost:8000')
-    assert links == []
-
-
-def test_links_css_static():
-    f = BytesIO(b"""
-        @import url('static/style.css');
-        body {
-            background-color: #C2F5FF;
-        }
-    """)
-    links = get_links_from_css(f, 'http://localhost:8000')
-    assert sorted(links) == ['http://localhost:8000/static/style.css']
-
-
-def test_links_css_one_external():
-    f = BytesIO(b"""
-        @font-face {
-            font-family: 'TurretRoad';
-            src: url(TurretRoad-Regular.ttf) format('truetype');
-            font-weight: normal;
-        }
-
-        body {
-            font-family: 'TurretRoad';
-            background-image: url('https://www.example.com/some-image.png');
-        }
-    """)
-    links = get_links_from_css(f, 'http://localhost:8000')
-    assert sorted(links) == [
-        'http://localhost:8000/TurretRoad-Regular.ttf',
-        'https://www.example.com/some-image.png'
-    ]
-
-
-def test_links_css_all_external():
-    f = BytesIO(b"""
-        @import url('https://www.example.com/style.css');
-
-        body {
-            background-image: url('https://www.example.com/some-image.png');
-        }
-    """)
-    links = get_links_from_css(f, 'http://localhost:8000')
-    assert sorted(links) == [
-        'https://www.example.com/some-image.png',
-        'https://www.example.com/style.css'
-    ]
+    assert sorted(links) == expected
