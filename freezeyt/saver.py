89904f1ba01ea0933036f138165dfc7b69ff9511
Petr Viktorin <encukou@gmail.com>
2022-04-19 19:46:57 +0200

    Use abc.ABC for Saver

--- freezeyt/saver.py
+++ freezeyt/saver.py
@@ -1,24 +1,26 @@
-
+import abc
 from pathlib import PurePosixPath
 from typing import BinaryIO, Any
 from collections.abc import Iterable
 
-class Saver:
+class Saver(abc.ABC):
     async def prepare(self):
         """Initialize the saver."""
 
+    @abc.abstractmethod
     async def save_to_filename(
         self,
         filename: PurePosixPath,
         content_iterable: Iterable[bytes],
     ) -> None:
-        raise NotImplementedError('{type(self)} needs to implement save_to_filename')
+        """Save the given bytes to the given path"""
 
+    @abc.abstractmethod
     async def open_filename(
         self,
         filename: PurePosixPath,
     ) -> BinaryIO:
-        raise NotImplementedError('{type(self)} needs to implement save_to_filename')
+        """Open the given path for reading bytes"""
 
     async def finish(self, success: bool, cleanup: bool) -> Any:
         """Clean up after a freeze and return the result, if any.
