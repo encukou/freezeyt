8a0fd197f3cf50be3c22470dc8dd4171fd2643e4
Petr Viktorin <encukou@gmail.com>
2022-06-28 18:19:04 +0200

    Run middleware tests with all test apps

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,17 +1,23 @@
-
 from werkzeug.test import Client
+
+import pytest
 
 from freezeyt.middleware import Middleware
 
-from fixtures.app_simple.app import app
+from testutil import APP_NAMES, context_for_test
 
-def test_middleware_doesnt_change_app():
-    app_client = Client(app)
-    mw_client = Client(Middleware(app, {}))
 
-    app_response = app_client.get('/')
-    mw_response = mw_client.get('/')
+@pytest.mark.parametrize('app_name', APP_NAMES)
+def test_middleware_doesnt_change_app(app_name):
+    with context_for_test(app_name) as module:
+        app = module.app
 
-    assert app_response.status == mw_response.status
-    assert app_response.headers == mw_response.headers
-    assert app_response.get_data() == mw_response.get_data()
+        app_client = Client(app)
+        mw_client = Client(Middleware(app, {}))
+
+        app_response = app_client.get('/')
+        mw_response = mw_client.get('/')
+
+        assert app_response.status == mw_response.status
+        assert app_response.headers == mw_response.headers
+        assert app_response.get_data() == mw_response.get_data()
