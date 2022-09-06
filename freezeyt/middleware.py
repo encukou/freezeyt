e74a0357623cabb77a55dd2ec3e09260b0e2ac54
Petr Viktorin <encukou@gmail.com>
2022-09-06 19:44:44 +0200

    Define a no-op start_response function for the middleware

--- freezeyt/middleware.py
+++ freezeyt/middleware.py
@@ -3,5 +3,10 @@ class Middleware:
     def __init__(self, app):
         self.app = app
 
-    def __call__(self, environ, start_response):
-        return self.app(environ, start_response)
+    def __call__(self, environ, server_start_response):
+
+        def mw_start_response(status, headers, exc_info=None):
+            result = server_start_response(status, headers, exc_info)
+            return result
+
+        return self.app(environ, mw_start_response)
