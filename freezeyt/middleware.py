52670b850c617e182445ef51aafb6c0de2952136
Petr Viktorin <encukou@gmail.com>
2022-09-20 19:54:14 +0200

    Add a few typing hints from wsgiref.types
    See: https://docs.python.org/3.11/library/wsgiref.html#module-wsgiref.types

--- freezeyt/middleware.py
+++ freezeyt/middleware.py
@@ -1,3 +1,5 @@
+from freezeyt.compat import StartResponse, WSGIEnvironment
+
 from freezeyt.mimetype_check import MimetypeChecker
 
 
@@ -6,7 +8,11 @@ class Middleware:
         self.app = app
         self.mimetype_checker = MimetypeChecker(config)
 
-    def __call__(self, environ, server_start_response):
+    def __call__(
+        self,
+        environ: WSGIEnvironment,
+        server_start_response: StartResponse,
+    ):
 
         path_info = environ.get('PATH_INFO', '')
 
