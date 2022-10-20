a0b76740eff4bc0158acbe881cb0ef469a7d55c0
jiri.one <nemec@jiri.one>
2022-10-20 14:22:16 +0200

    added test for rmtree onerror parametr

--- tests/test_remove_output_dir.py
+++ tests/test_remove_output_dir.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+import shutil
 
 from freezeyt import freeze, DirectoryExistsError
 
@@ -56,3 +58,47 @@ def test_overwrite_dir_with_index(tmp_path):
 
     assert not old_path.exists()
     assert not old_dir_path.exists()
+
+
+def test_rm_output_dir_with_protected_file(tmp_path):
+    """We test that if there is an index.html in the directory and a file with permissions 000 (protected file), the directory will be overwritten successfully."""
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    protected_file = output_dir / "protected.file"
+    protected_file.touch()
+    protected_file.chmod(0o000)
+    index_path = output_dir / 'index.html'
+    index_path.write_text('<html></html>')
+    config = {
+        **freeze_config,
+        'output': {'type': 'dir', 'dir': output_dir},
+    }
+    freeze(app, config)
+    assert not protected_file.exists()
+
+
+def test_rm_output_dir_with_protected_file_on_windows_fail(tmp_path, monkeypatch):
+    """We test that if there is an index.html file in the directory and a file with permissions 000 (protected file) is in dir too, then if we use rmtree without the onerror parameter, which otherwise calls the internal add_write_flag method, an exception is raised on Windows systems (os.name == 'nt'). On other systems, the deletion should be successful without the internal add_write_flag method too."""
+    from shutil import rmtree as rmtree_for_mocking
+    def mock_rmtree(path, onerror=None):
+        """Mocked rmtree method, which only get rid of the onerror parameter."""
+        return rmtree_for_mocking(path)
+    monkeypatch.setattr(shutil, "rmtree", mock_rmtree)
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    protected_file = output_dir / "protected.file"
+    protected_file.touch()
+    protected_file.chmod(0o000)
+    index_path = output_dir / 'index.html'
+    index_path.write_text('<html></html>')
+    config = {
+        **freeze_config,
+        'output': {'type': 'dir', 'dir': output_dir},
+    }
+    if os.name == "nt":
+        with pytest.raises(PermissionError):
+            freeze(app, config)
+        assert protected_file.exists()
+    else:
+        freeze(app, config)
+        assert not protected_file.exists()
