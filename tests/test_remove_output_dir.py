35b6697f3b3d9db9523504e3376cc403f16d9ecc
jiri.one <nemec@jiri.one>
2022-10-31 18:41:45 +0100

    changed to test_add_write_flag_on_windows

--- tests/test_remove_output_dir.py
+++ tests/test_remove_output_dir.py
@@ -1,8 +1,10 @@
+from msilib.schema import File
 import pytest
 import os
 import shutil
 
 from freezeyt import freeze, DirectoryExistsError
+from freezeyt.filesaver import FileSaver
 
 from fixtures.app_with_extra_files.app import app, freeze_config
 
@@ -77,33 +79,20 @@ def test_rm_output_dir_with_protected_file(tmp_path):
     freeze(app, config)
     assert not protected_file.exists()
 
-
-def test_rm_output_dir_with_protected_file_on_windows_fail(tmp_path, monkeypatch):
-    """We test that if there is an index.html file in the directory and a file with permissions
-    000 (protected file) is in dir too, then if we use rmtree without the onerror parameter,
-    which otherwise calls the internal add_write_flag method, an exception is raised on Windows
-    systems (os.name == 'nt'). On other systems, the deletion should be successful without the
-    internal add_write_flag method too."""
-    from shutil import rmtree as rmtree_for_mocking
-    def mock_rmtree(path, onerror=None):
-        """Mocked rmtree method, which only get rid of the onerror parameter."""
-        return rmtree_for_mocking(path)
-    monkeypatch.setattr(shutil, "rmtree", mock_rmtree)
-    output_dir = tmp_path / "output"
-    output_dir.mkdir()
-    protected_file = output_dir / "protected.file"
-    protected_file.touch()
-    protected_file.chmod(0o000)
-    index_path = output_dir / 'index.html'
-    index_path.write_text('<html></html>')
-    config = {
-        **freeze_config,
-        'output': {'type': 'dir', 'dir': output_dir},
-    }
-    if os.name == "nt":
-        with pytest.raises(PermissionError):
-            freeze(app, config)
+if os.name == "nt": # this test will run only on Windows systems
+    def test_add_write_flag_on_windows(tmp_path):
+        """We are testing that we need to use the add_write_flag method (as an onerror
+        parameter to the rmtree method) on Windows, otherwise rmtree on Windows will not be
+        able to delete protected files in output dir - for example, some files in the .git
+        directory."""
+        output_dir = tmp_path / "output"
+        output_dir.mkdir()
+        protected_file = output_dir / "protected.file"
+        protected_file.touch()
+        protected_file.chmod(0o000)
+        with pytest.raises(PermissionError): # without onerror=add_write_flag
+            shutil.rmtree(output_dir)
         assert protected_file.exists()
-    else:
-        freeze(app, config)
+        shutil.rmtree(output_dir, onerror=FileSaver.add_write_flag)
+        assert not output_dir.exists()
         assert not protected_file.exists()
