c38861ae77128b043e070b682b91d197d7f283a2
dstroch <d.stroch@gmail.com>
2023-01-15 21:57:25 +0100

    Replace TypeErros to ValueErrors

--- tests/fixtures/app_fail_fast/app.py
+++ tests/fixtures/app_fail_fast/app.py
@@ -14,17 +14,17 @@ def index():
         </head>
         <body>
             Hello world!
-            <a href="{url_for('type_error')}">Link</a>
-            <a href="{url_for('type_error2')}">Link</a>
+            <a href="{url_for('value_error')}">Link</a>
+            <a href="{url_for('value_error2')}">Link</a>
         </body>
     </html>
     """
 
-@app.route('/type_error')
-def type_error():
-    raise TypeError()
+@app.route('/value_error')
+def value_error():
+    raise ValueError()
 
-@app.route('/type_error2')
-def type_error2():
-    raise TypeError()
+@app.route('/value_error2')
+def value_error2():
+    raise ValueError()
 
