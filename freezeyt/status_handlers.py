35868cfbd2e4c0e001c735ccdedf1d0c84879919
Petr Viktorin <encukou@gmail.com>
2023-04-04 19:12:28 +0200

    Add status_handlers.__all__ to appease pyflakes

--- freezeyt/status_handlers.py
+++ freezeyt/status_handlers.py
@@ -3,6 +3,11 @@ import warnings
 from freezeyt.actions import warn, follow, ignore, save, error
 from freezeyt.actions import ActionFunction as StatusHandler
 
+__all__ = [
+    'warn', 'follow', 'ignore', 'save', 'error',
+    'StatusHandler',
+]
+
 warnings.warn(
     'The status_handlers module is deprecated, use freezeyt.actions',
     DeprecationWarning,
