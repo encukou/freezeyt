5e5cb7ee87b2d213488c4f664d990b38c95634b8
dstroch <d.stroch@gmail.com>
2023-05-22 15:15:24 +0200

    Test url_parts with slashes

--- tests/test_extra_files.py
+++ tests/test_extra_files.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 from freezeyt.extra_files import get_extra_files
+from freezeyt import freeze
 
-from testutil import FIXTURES_PATH
+from testutil import FIXTURES_PATH, context_for_test
 
 IMAGE_BYTES = b'\x89PNG\r\n\x1a\n\0\0\0\rIHDR\0\0\0\x08\0\0\0\x08\x08\x04\0\0\0n\x06v\0\0\0\0#IDAT\x08\xd7cd``\xf8\xcf\x80\0\x8c\xa8\\ \x8f\tB!\x91D\xab\xf8\x8f\x10D\xd3\xc2\x88n-\0\x0e\x1b\x0f\xf9LT9_\0\0\0\0IEND\xaeB`\x82'
 
@@ -13,6 +14,27 @@ def test_simple():
         }
     }
     assert list(get_extra_files(config)) == [('foo', 'content', b'abc')]
+
+
+def test_slashes():
+    config = {
+        'extra_files': {
+            'a': 'a',
+            '/b': 'b',
+            '/c/': 'c',
+            '/d//': 'd',
+            '//e/': 'e',
+            '/f//g/': 'fg',
+        },
+        'output': {'type': 'dict'},
+    }
+
+    with context_for_test('app_simple') as module:
+        result = freeze(module.app, config)
+
+    result.pop('index.html')
+    assert result == {}
+
 
 def test_content():
     config = {
