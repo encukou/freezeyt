8a04a23928100b1b3ffc82ac6be07e1840838202
Petr Viktorin <encukou@gmail.com>
2023-11-21 18:12:38 +0100

    dictsaver: Remove duplication

--- freezeyt/dictsaver.py
+++ freezeyt/dictsaver.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from pathlib import PurePosixPath
-from typing import Dict, Union, Iterable
+from typing import Dict, Union, Iterable, Tuple
 
 from .saver import Saver
 
@@ -9,7 +9,7 @@ from .saver import Saver
 DictSaverContents = Dict[
     str,  # filename; maps to either:
     Union[
-        bytes,  # file contents, or
+        bytes,  # file content, or
         'DictSaverContents',  # a subdirectory.
     ]
 ]
@@ -18,51 +18,55 @@ class DictSaver(Saver):
     """Outputs frozen pages into a dict.
     """
     def __init__(self):
-        self.contents: DictSaverContents = {}
+        self.root_dir: DictSaverContents = {}
 
     async def save_to_filename(
         self,
-        filename: PurePosixPath,
+        filepath: PurePosixPath,
         content_iterable: Iterable[bytes],
     ) -> None:
-        path = PurePosixPath(filename)
-        if path.is_absolute():
-            raise ValueError('filename must be relative')
-        parts = path.parts
-        if not parts:
-            raise IsADirectoryError('.')
+        parent_dir, name = get_parent_and_name(self.root_dir, filepath)
 
-        target: DictSaverContents = self.contents
-        for part in parts[:-1]:
-            new_target = target.setdefault(part, {})
-            if not isinstance(new_target, dict):
-                raise NotADirectoryError(PurePosixPath(*parts[:-1]))
-            target = new_target
-        if isinstance(target.get(parts[-1]), dict):
-            raise IsADirectoryError(filename)
-        target[parts[-1]] = b''.join(content_iterable)
+        if isinstance(parent_dir.get(name), dict):
+            raise IsADirectoryError(filepath)
+        parent_dir[name] = b''.join(content_iterable)
 
-    async def open_filename(self, filename: PurePosixPath) -> BytesIO:
-        path = PurePosixPath(filename)
-        if path.is_absolute():
-            raise ValueError('filename must be relative')
-        parts = path.parts
-        if not parts:
-            raise IsADirectoryError('.')
+    async def open_filename(self, filepath: PurePosixPath) -> BytesIO:
+        parent_dir, name = get_parent_and_name(self.root_dir, filepath)
 
-        target: DictSaverContents = self.contents
-        for part in parts[:-1]:
-            new_target = target.setdefault(part, {})
-            if not isinstance(new_target, dict):
-                raise NotADirectoryError(PurePosixPath(*parts[:-1]))
-            target = new_target
         try:
-            contents = target[parts[-1]]
+            file_content = parent_dir[name]
         except KeyError:
-            raise FileNotFoundError(filename)
-        if isinstance(contents, dict):
-            raise IsADirectoryError(filename)
-        return BytesIO(contents)
+            raise FileNotFoundError(filepath)
+        if isinstance(file_content, dict):
+            raise IsADirectoryError(filepath)
+        return BytesIO(file_content)
 
     async def finish(self, success: bool, cleanup: bool) -> DictSaverContents:
-        return self.contents
+        return self.root_dir
+
+def get_parent_and_name(
+    root_dir: DictSaverContents,
+    filepath: PurePosixPath,
+) -> Tuple[DictSaverContents, str]:
+    """Return a dict representing the parent directory of the given file, and
+    the name of the file.
+
+    root_dir: a dict representing the root directory (DictSaver.root_dir)
+    filepath: relative path to the file
+    """
+    path = PurePosixPath(filepath)
+    if path.is_absolute():
+        raise ValueError('filepath must be relative')
+    parts = path.parts
+    if not parts:
+        raise IsADirectoryError('.')
+
+    current_directory: DictSaverContents = root_dir
+    for part in parts[:-1]:
+        new_curdir = current_directory.setdefault(part, {})
+        if not isinstance(new_curdir, dict):
+            raise NotADirectoryError(PurePosixPath(*parts[:-1]))
+        current_directory = new_curdir
+
+    return current_directory, parts[-1]
