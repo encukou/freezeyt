87e92e568a9f00e091a1c5058a58b866d64f2f5f
dstroch <d.stroch@gmail.com>
2024-01-16 15:01:22 +0100

    Test redirect to itself with different URL

---
+++ tests/test_handle_redirects.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response, url_for
+
+from freezeyt import freeze
+
+def test_redirect_to_itself_by_different_URL():
+    """WSGI app can define for one static file
+    different URLs with different content (e.g. '/', '/index.html'),
+    It is complicated for Freezeyt as static server to disntict
+    two different URLs for one static file.
+    One content will be always lost.
+    In case of redirection ..... ????????
+    """
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return Response(
+        "Redirecting to /index.html",
+        status='301 Moved Permanently',
+        headers=[('Location', url_for('index_html'))],
+    )
+
+    @app.route('/index.html')
+    def index_html():
+        return "Hello world!"
+
+    config = {'output': {'type': 'dict'}}
+
+    result=freeze(app, config)
+
+    expected = {'index.html': b"Hello world!"}
+
+    assert result == expected
+
+
