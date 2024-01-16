48663c870569e6de1a5c699b436cde86e967e1af
dstroch <d.stroch@gmail.com>
2024-01-16 15:08:31 +0100

    Test redirect to itself with double slash hop

--- tests/test_handle_redirects.py
+++ tests/test_handle_redirects.py
@@ -58,3 +58,37 @@ def test_infinite_redirect_to_itself_by_different_URL():
     freeze(app, config)
 
 
+def test_redirect_to_itself_by_different_URL_with_double_slash_hop():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return Response(
+            "Redirecting to //",
+            status='301 Moved Permanently',
+            headers=[('Location', url_for('index_double_slash'))],
+    )
+
+    @app.route('//')
+    def index_double_slash():
+        return Response(
+            "Redirecting to /index.html",
+            status='301 Moved Permanently',
+            headers=[('Location', url_for('index_html'))],
+        )
+
+    @app.route('/index.html')
+    def index_html():
+        return "Hello world!"
+
+    config = {
+        'output': {'type': 'dict'},
+    }
+
+    result=freeze(app, config)
+
+    expected = {'index.html': b"Hello world!"}
+
+    assert result == expected
+
+
