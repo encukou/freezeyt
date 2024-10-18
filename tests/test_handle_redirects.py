299d274143697e2e0879dc89cbb1e39787ae0e02
dstroch <d.stroch@gmail.com>
2024-10-18 17:58:22 +0200

    Fix docstrings

--- tests/test_handle_redirects.py
+++ tests/test_handle_redirects.py
@@ -2,13 +2,9 @@ from flask import Flask, redirect, url_for, request
 
 from freezeyt import freeze
 
-    """WSGI app can define for one static file
-    different URLs with different content (e.g. '/', '/index.html'),
-    It is complicated for Freezeyt as static server to disntict
-    two different URLs for one static file.
-    One content will be always lost.
-    In case of redirection ..... ????????
 def test_redirect_to_same_static_file():
+    """One URL redirects to next URL which freeze
+    page to same filepath as would be freeyzed by first URL.
     """
 
     app = Flask(__name__)
@@ -32,6 +28,10 @@ def test_redirect_to_same_static_file():
 
 
 def test_infinite_redirect_to_same_static_file():
+    """Two different URLs redirect to same freezed file,
+    then infinite loop is processing, so first URL redirect
+    to latter and vice versa.
+    """
     app = Flask(__name__)
 
     @app.route('/')
@@ -50,6 +50,10 @@ def test_infinite_redirect_to_same_static_file():
 
 
 def test_redirect_to_same_static_file_with_double_slash_hop():
+    """Add to redirecting tracerout a node with double slash URL.
+    Finally, the double slash URL redirects to page where is some
+    content.
+    """
     app = Flask(__name__)
 
     @app.route('/')
@@ -78,6 +82,10 @@ def test_redirect_to_same_static_file_with_double_slash_hop():
 
 
 def test_redirect_to_same_static_file_with_query_hop():
+    """Add a redirect whose URL contains query parameter.
+    Redirect URL with query parameter is prepared by
+    first request of index page to app.
+    """
     app = Flask(__name__)
 
     @app.route('/')
@@ -106,6 +114,9 @@ def test_redirect_to_same_static_file_with_query_hop():
 
 
 def test_redirect_to_same_static_file_with_hop():
+    """Add a new URL with no relation to index
+    between URLs, which are freezed to same file.
+    """
     app = Flask(__name__)
 
     @app.route('/')
