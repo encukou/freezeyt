73a282f2978ef27f0ca0d564555ed9328790ec28
Petr Viktorin <encukou@gmail.com>
2025-07-08 18:49:37 +0200

    Consolidate common URL behaviour

--- freezeyt/absolute_url.py
+++ freezeyt/absolute_url.py
@@ -21,38 +21,21 @@ def get_port(port, scheme):
         return 443
 
 
-class PrefixURL:
-    """An URL as used internally by Freezeyt.
-
-    Absolute `http` or `https` IRI, with an explicit port, ending with a slash.
-    For example:
-        https://localhost:80/some-path/
+class BaseURL:
+    """An absolute IRI as used internally by Freezeyt
     """
-    def __init__(self, str_or_splitresult: Union[str, urllib.parse.SplitResult], /):
-        _url = str_or_splitresult
+    prefix: 'PrefixURL'
+
+    def __init__(
+        self,
+        str_or_splitresult: Union[str, urllib.parse.SplitResult],
+        /,
+    ):
         split_url: urllib.parse.SplitResult
         if isinstance(str_or_splitresult, str):
             split_url = split_iri(str_or_splitresult)
         else:
             split_url = str_or_splitresult
-        if not split_url.scheme:
-            raise RelativeURLError(f"Expected an absolute URL, not {_url}")
-
-        if split_url.scheme not in ('http', 'https'):
-            raise UnsupportedSchemeError(f"URL scheme must be http or https: {_url}")
-
-        if not split_url.netloc:
-            raise RelativeURLError(f"Expected an absolute URL, not {_url}")
-
-        if split_url.query:
-            raise BadPrefixError("The prefix cannot have a query part")
-
-        if split_url.fragment:
-            raise BadPrefixError("The prefix cannot have a fragment part")
-
-        if not split_url.path.endswith('/'):
-            raise BadPrefixError("The prefix must end with a slash")
-
         self._split_url = split_url
 
     def __str__(self):
@@ -78,21 +61,63 @@ class PrefixURL:
     def path(self):
         return self._split_url.path
 
+    @property
+    def query(self):
+        return self._split_url.query
+
+    def join(self, link_text: str) -> 'AppURL':
+        """Add a string to the URL, as if following a link with the given href
+        """
+        url_text = urllib.parse.urlunsplit(self._split_url)
+        result_text = urllib.parse.urljoin(url_text, uri_to_iri(link_text))
+        result = urllib.parse.urlsplit(result_text)
+        return AppURL(result, self.prefix)
+
+
+class PrefixURL(BaseURL):
+    """IRI used for a web app's prefix.
+
+    Absolute `http` or `https` IRI, with an explicit port, ending with a slash.
+    For example:
+        https://localhost:80/some-path/
+    """
+    def __init__(self, str_or_splitresult: Union[str, urllib.parse.SplitResult], /):
+        super().__init__(str_or_splitresult)
+        _url = str_or_splitresult
+        split_url = self._split_url
+        if not split_url.scheme:
+            raise RelativeURLError(f"Expected an absolute URL, not {_url}")
+
+        if split_url.scheme not in ('http', 'https'):
+            raise UnsupportedSchemeError(
+                f"URL scheme must be http or https: {_url}",
+            )
+
+        if not split_url.netloc:
+            raise RelativeURLError(f"Expected an absolute URL, not {_url}")
+
+        if split_url.query:
+            raise BadPrefixError("The prefix cannot have a query part")
+
+        if split_url.fragment:
+            raise BadPrefixError("The prefix cannot have a fragment part")
+
+        if not split_url.path.endswith('/'):
+            raise BadPrefixError("The prefix must end with a slash")
+
+        self._split_url = split_url
+        self.prefix = self
+
     def _replace_path(self, path):
         return PrefixURL(self._split_url._replace(path=path))
 
     def as_app_url(self):
         return AppURL(self._split_url, self)
 
-    def join(self, link_text: str) -> 'AppURL':
-        return self.as_app_url().join(link_text)
-
 
 @functools.total_ordering
-class AppURL:
-    """An URL as used internally by Freezeyt.
-
-    An absolute IRI that's "internal" to a given prefix.
+class AppURL(BaseURL):
+    """A IRI that's "internal" to a given prefix.
     """
     def __init__(
         self,
@@ -101,12 +126,8 @@ class AppURL:
         prefix: PrefixURL,
     ):
         _url = str_or_splitresult
-        split_url: urllib.parse.SplitResult
-        if isinstance(str_or_splitresult, str):
-            split_url = split_iri(str_or_splitresult)
-        else:
-            split_url = str_or_splitresult
-        split_url = split_url._replace(fragment='')
+        super().__init__(str_or_splitresult)
+        split_url = self._split_url._replace(fragment='')
         if split_url.scheme != prefix.scheme:
             raise ExternalURLError(
                 f"External URL: {_url!r} (scheme is not {prefix.scheme!r})")
@@ -130,24 +151,6 @@ class AppURL:
         self._split_url = split_url
         self.prefix = prefix
 
-    def __str__(self):
-        return urllib.parse.urlunsplit(self._split_url)
-
-    @property
-    def path(self):
-        return self._split_url.path
-
-    def join(self, link_text: str) -> 'AppURL':
-        """Add a string to the URL, adding a default port for http/https"""
-        url_text = urllib.parse.urlunsplit(self._split_url)
-        result_text = urllib.parse.urljoin(url_text, uri_to_iri(link_text))
-        result = urllib.parse.urlsplit(result_text)
-        return AppURL(result, self.prefix)
-
-    @property
-    def query(self):
-        return self._split_url.query
-
     @property
     def _key(self):
         return self.relative_path, self.query, self.prefix
@@ -160,10 +163,6 @@ class AppURL:
 
     def __hash__(self):
         return hash(self._key)
-
-    @property
-    def port(self):
-        return get_port(self._split_url.port, self._split_url.scheme)
 
     @property
     def relative_path_with_query(self):
